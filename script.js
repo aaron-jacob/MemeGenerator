48a93ee6b248bd07cc0a5ca36d0663f88089b094
Aaron Jacob <aaronjacobaj4@gmail.com>
2023-10-08 19:36:52 +0530

    commiting all files to repo

---
+++ script.js
@@ -0,0 +1,26 @@
+let meme = document.getElementById("meme");
+let title = document.getElementById("title");
+let getMemeBtn = document.getElementById("get-meme-btn");
+
+let url = "https://meme-api.com/gimme/";
+
+let subreddits = ["catmemes", "memes"];
+
+let getMeme = () => {
+    let randomSub = subreddits[Math.floor(Math.random() * subreddits.length)];
+    
+    fetch(url+randomSub)
+    .then(resp => resp.json())
+    .then((data) => {
+        let memeImg = new Image();
+
+        memeImg.onload = () => {
+            meme.src = data.url;
+            title.innerHTML = data.title;
+        };
+        memeImg.src = data.url;
+    });
+}
+
+getMemeBtn.addEventListener("click", getMeme);
+window.addEventListener("load", getMeme);
